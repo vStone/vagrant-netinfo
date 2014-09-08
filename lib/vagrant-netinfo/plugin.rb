b6de6df5af4e9fcc4590cf251fdf9e1727dde463
Jan Vansteenkiste <jan@vstone.eu>
2014-09-08 13:44:06 +0200

    Don't fail but just warn on version check

--- lib/vagrant-netinfo/plugin.rb
+++ lib/vagrant-netinfo/plugin.rb
@@ -5,7 +5,8 @@ rescue LoadError
 end
 
 if Vagrant::VERSION < '1.6.0'
-  raise 'The Vagrant Netinfo plugin has only been tested with vagrant 1.6.x'
+  puts 'WARNING: The Vagrant Netinfo plugin has only been tested with vagrant 1.6.x'
+  puts 'If it works with your version of vagrant, send me a pull request updating this version check ;)'
 end
 
 module VagrantPlugins
